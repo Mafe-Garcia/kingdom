39172d6ef4ca6e9c94d2c162a15084df92f0d1db
Mafe-Garcia <mariaf.garcia@upb.edu.co>
2023-10-20 09:16:43 -0500

    oleadas
    se agregaron mil scripts

---
+++ Assets/JuegoUPB/Scripts/Oleada/Ola.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using MoreMountains.Tools;
+using UnityEngine;
+
+public class Ola : MonoBehaviour
+{
+    [SerializeField]
+    private List<OlaData> data;
+    private GameObject grupos;
+
+    [SerializeField]
+    //Esta es la ruta que seguiran todos los enemigos de la ola
+    [Header("Debug")]
+    [MMInspectorButton("AgregarUnEnemigo")]
+    /// a test button to spawn an object
+    public bool PuedeAgregarButton;
+
+    public void Start()
+    {
+        grupos = new GameObject("Grupos");
+        grupos.transform.SetParent(this.transform);
+        CrearGrupos();
+    }
+
+    public void CrearGrupos()
+    {
+        OlaGrupo temp;
+
+        foreach (OlaData d in data)
+        {
+            temp = grupos.AddComponent<OlaGrupo>();
+            temp.CrearGrupo(d);
+            temp.Tiempo_salida = d.TiempoSalida;
+        }
+    }
+
+    //Metodo de prueba desde el inspector que agrega Koalas
+    public void AgregarUnEnemigo()
+    {
+        //AgregarEnemigo(new OlaData(TipoEnemigo.Koala));
+    }
+
+}
---
+++ Assets/JuegoUPB/Scripts/Oleada/Oleadas.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oleadas : MonoBehaviour
+{
+    public static Oleadas Instance { get; private set; }
+
+    private void Awake()
+    {
+        // If there is an instance, and it's not me, delete myself.
+
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public void Start()
+    {
+        //Debug.Log("Se asignan los caminos");
+    }
+
+    public Enemigo BuscarEnemigoDisponible(TipoEnemigo tipo)
+    {
+        return null;
+    }
+}
